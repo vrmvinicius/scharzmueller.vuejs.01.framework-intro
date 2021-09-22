7f640ea8fd5deed4fadccd4cd9d097642dd5ceed
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-09-22 14:09:44 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+     <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/blog">Blog</router-link>
+        <router-link to="/account">Account</router-link>
+     </div>
      <router-view></router-view>
      <!-- <app-user></app-user> -->
      <p>This isn't Italic now!</p>
