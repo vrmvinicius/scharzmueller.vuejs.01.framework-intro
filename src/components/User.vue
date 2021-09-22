94ff9a5f1817a8f2f92bdab27e40a520ad4284ac
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-09-22 11:18:28 -0300

    .

---
+++ src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+   <div id="user">
+      <h3>The User</h3>
+      <p>My username is {{ username }}</p>
+   </div>
+</template>
+
+<script>
+   export default{
+      data(){
+         return {
+            username: 'Vinicius'
+         }
+      }
+   }
+</script>
+
+<style>
+   p{
+      font-style: italic;
+   }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import User from './components/User.vue';
+
+Vue.component('app-user', User); 
 
 new Vue({
   el: '#app',
